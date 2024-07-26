4c34e9754f155eaf146354a267e76174e20002d0
nimanikoo <n.nikoonazar@azki.com>
2024-07-26 20:49:22 +0330

    Add Processors and Orchestrator Projects

---
+++ src/OrderProcessor/Program.cs
@@ -0,0 +1,7 @@
+using OrderProcessor;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
---
+++ src/PricingProcessor/Program.cs
@@ -0,0 +1,7 @@
+using PricingProcessor;
+
+var builder = Host.CreateApplicationBuilder(args);
+builder.Services.AddHostedService<Worker>();
+
+var host = builder.Build();
+host.Run();
